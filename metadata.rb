6cb6eec9a12b670e2c6c82a54304159a79594083
Ivan Golman <ivan.golman@gmail.com>
2016-06-21 21:01:57 +0200

    foodcritic compliance made.

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          'MIT License'
 description      'Installs/Configures ntpdate'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.0'
+issues_url 'https://github.com/igolman/ntpdate/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/igolman/ntpdate' if respond_to?(:source_url)
 
 %w{ debian ubuntu }.each do |os|
   supports os
--- recipes/install.rb
+++ recipes/install.rb
@@ -1,13 +1,12 @@
 # should I remove installed NTP Packet ( activated NTP service leads to race condition on network port).
 
-if node[:ntpdate][:ntp_pkg_remove]
-	package node[:ntpdate][:ntp_pkg_name] do
-		action node[:ntpdate][:enable] ? :remove : :nothing
-	end
+package node['ntpdate']['ntp_pkg_name'] do
+  action node['ntpdate']['enable'] ? 'remove' : 'nothing'
+  only_if {node['ntpdate']['ntp_pkg_remove']}
 end
 
-node[:ntpdate][:package_name].each do |pkg|
+node['ntpdate']['package_name'].each do |pkg|
   package pkg do
-    action node[:ntpdate][:enable] ? :install : :nothing
+    action node['ntpdate']['enable'] ? 'install' : 'nothing'
   end
 end
